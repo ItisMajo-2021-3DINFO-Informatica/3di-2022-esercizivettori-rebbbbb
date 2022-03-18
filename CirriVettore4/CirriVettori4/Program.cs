b433c9140cc23b0b7cfaf52a8801486479307b4a
rebb <98151576+rebbbbb@users.noreply.github.com>
2022-03-18 10:43:53 +0100

    EserciziVettori

---
+++ CirriVettore4/CirriVettori4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CirriVettori4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Crea una app che carica un vettore di 100 elementi con valori casuali compresi tra 1000 e 3000
+               e stampi il più grande ed il più piccolo dei valori contenuti nel vettore */
+
+
+            Console.WriteLine("Esercizio vettori 4°");
+
+            const int dimensione = 100;
+            int[] vettore = new int[dimensione];
+
+            Random generatore = new Random();
+
+            for (int i = 0; i < dimensione; i++)
+            {
+                vettore[i] = generatore.Next(1000, 3001);
+
+                if ()
+            }
+
+            for (int i = 0; i < dimensione; i++)
+            {
+                Console.WriteLine(vettore[i]);
+            }
+        }
+    }
+}
---
+++ CirriVettore5/CirriVettore5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CirriVettore5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crea una app che carica un vettore di 1000 elementi con valori casuali compresi tra 1 e 250
+              e stampi quanti elementi in percentuale sono minori di 175*/
+
+            Console.WriteLine("Esercizio vettori 5°");
+
+            const int dimensione = 1000;
+            int[] vettore = new int[dimensione];
+            int percentuale = Convert.ToInt32(Console.ReadLine());
+
+            Random generatore = new Random();
+
+            for (int i = 0; i < dimensione; i++)
+            {
+                vettore[i] = generatore.Next(1, 251);
+            }
+
+            for (int i = 0; i > dimensione; i++)
+            {
+                percentuale = vettore[i] / 100;
+
+                if (percentuale < 175)
+                {
+                    Console.WriteLine("Il numero è minore di 175");
+                }
+            }
+
+            /* for (int i = 0; i < dimensione; i++)
+            {
+                Console.WriteLine(vettore[i]);
+            } */
+        }
+    }
+}
